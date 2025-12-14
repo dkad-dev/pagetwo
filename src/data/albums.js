4e639fdf10b263159d02f3d93e3d13d6757ca8ae
Douglas Kadlec <colorghost@outlook.com>
2025-12-14 12:32:30 +0700

    Add albums to discography

---
+++ src/data/albums.js
@@ -0,0 +1,462 @@
+export const albums = [
+	{
+		title: 'READY TO BE',
+		release: 'March 10, 2023',
+		art: 'readytobe',
+		tracklist: [
+			'SET ME FREE',
+			'MOONLIGHT SUNRISE',
+			'GOT THE THRILLS',
+			'BLAME IT ON ME • Dahyun',
+			'WALLFLOWER',
+			'CRAZY STUPID LOVE • Dahyun',
+			'SET ME FREE (ENG)'
+		]
+	},
+	{
+		title: 'BETWEEN 1&2',
+		release: 'August 26, 2022',
+		art: 'between1&2',
+		tracklist: [
+			'Talk That Talk',
+			'Queen of Hearts',
+			'Basics • Chaeyoung',
+			'Trouble • Jihyo',
+			'Brave',
+			'Gone • Dahyun',
+			'When We Were Kids • Dahyun'
+		]
+	},
+	{
+		title: 'Celebrate',
+		release: 'July 27, 2022',
+		art: 'celebrate',
+		tracklist: [
+			'Celebrate • TWICE',
+			'Voices of Delight',
+			'Tick Tock • Dahyun',
+			'Flow Like Waves',
+			"That's All I'm Saying • Dahyun",
+			'Bitter Sweet',
+			'Sandcastle',
+			'Just Be Yourself',
+			'Doughnut'
+		]
+	},
+	{
+		title: 'IM NAYEON',
+		release: 'June 24, 2022',
+		art: 'imnayeon',
+		tracklist: [
+			'POP!',
+			'NO PROBLEM',
+			'LOVE COUNTDOWN • Nayeon',
+			'CANDYFLOSS',
+			'ALL OR NOTHING • Nayeon',
+			'HAPPY BIRTHDAY TO YOU',
+			'SUNSET'
+		]
+	},
+	{
+		title: '#TWICE4',
+		release: 'March 26, 2022',
+		art: 'twice4',
+		tracklist: [
+			"I CAN'T STOP ME (Japanese Ver.)",
+			'CRY FOR ME (Japanese Ver.)',
+			'Alcohol-Free (Japanese Ver.)',
+			'SCIENTIST (Japanese Ver.)'
+		]
+	},
+	{
+		title: 'Formula of Love',
+		release: 'November 12, 2021',
+		art: 'formulaoflove',
+		tracklist: [
+			'SCIENTIST',
+			'MOONLIGHT',
+			'ICON',
+			'CRUEL • Dahyun',
+			'REAL YOU • Jihyo',
+			'F.I.L.A (Fall In Love Again) • Nayeon',
+			'LAST WALTZ',
+			'ESPRESSO',
+			'REWIND',
+			'CACTUS • Jihyo',
+			'PUSH & PULL',
+			'HELLO',
+			'1,3,2',
+			'CANDY',
+			'The Feels',
+			'SCIENTIST (R3HAB Remix)'
+		]
+	},
+	{
+		title: 'Perfect World',
+		release: 'July 28, 2021',
+		art: 'perfectworld',
+		tracklist: [
+			'Perfect World',
+			'BETTER',
+			'Good at Love',
+			'Fanfare',
+			'Kura Kura',
+			'Four-leaf Clover',
+			'In the Summer',
+			'PIECES OF LOVE',
+			'Thank You, Family',
+			'PROMISE'
+		]
+	},
+	{
+		title: 'Taste of Love',
+		release: 'June 11, 2021',
+		art: 'tasteoflove',
+		tracklist: [
+			'Alcohol-Free',
+			'First Time • Jihyo',
+			'Scandal • Dahyun',
+			'Conversation • Sana',
+			'Baby Blue Love • Nayeon',
+			'SOS • Dahyun'
+		]
+	},
+	{
+		title: 'Eyes wide open',
+		release: 'October 26, 2020',
+		art: 'eyeswideopen',
+		tracklist: [
+			"I CAN'T STOP ME",
+			'HELL IN HEAVEN',
+			'UP NO MORE • Jihyo',
+			'DO WHAT WE LIKE • Sana',
+			'BRING IT BACK • Dahyun',
+			'BELIEVER',
+			'QUEEN • Dahyun',
+			'GO HARD',
+			'SHOT CLOCK',
+			'HANDLE IT • Chaeyoung',
+			'DEPEND ON YOU • Nayeon',
+			'SAY SOMETHING',
+			'BEHIND THE MASK'
+		]
+	},
+	{
+		title: '#TWICE3',
+		release: 'September 16, 2020',
+		art: 'twice3',
+		tracklist: [
+			'The Best Thing I Ever Did (Japanese Ver.)',
+			'FANCY (Japanese Ver.)',
+			'Feel Special (Japanese Ver.)',
+			'MORE & MORE (Japanese Ver.)',
+			'STUCK IN MY HEAD (Japanese Ver.)',
+			'21:29 (Japanese Ver.) • TWICE'
+		]
+	},
+	{
+		title: 'MORE & MORE',
+		release: 'June 1, 2020',
+		art: 'more&more',
+		tracklist: [
+			'MORE & MORE',
+			'OXYGEN',
+			'FIREWORK',
+			'MAKE ME GO • Nayeon',
+			'SHADOW',
+			"DON'T CALL ME AGAIN",
+			'SWEET SUMMER DAY • Jeongyeon, Chaeyoung'
+		]
+	},
+	{
+		title: '&TWICE -Repackage-',
+		release: 'Febuary 5, 2020',
+		art: '&twicerepackage',
+		tracklist: [
+			'SWING',
+			'Fake & True',
+			'Stronger',
+			'Breakthrough',
+			'Changing!',
+			'HAPPY HAPPY',
+			'What You Waiting For',
+			'Be OK',
+			'POLISH',
+			"How u doin' • Chaeyoung",
+			'The Reason Why'
+		]
+	},
+	{
+		title: '&TWICE',
+		release: 'November 19, 2019',
+		art: '&twice',
+		tracklist: [
+			'Fake & True',
+			'Stronger',
+			'Breakthrough',
+			'Changing!',
+			'HAPPY HAPPY',
+			'What You Waiting For',
+			'Be OK',
+			'POLISH',
+			"How u doin' • Chaeyoung",
+			'The Reason Why'
+		]
+	},
+	{
+		title: 'Feel Special',
+		release: 'September 23, 2019',
+		art: 'feelspecial',
+		tracklist: [
+			'Feel Special',
+			'RAINBOW • Nayeon',
+			'GET LOUD • Jihyo',
+			'TRICK IT • Dahyun',
+			'LOVE FOOLISH • Momo',
+			'21:29 • TWICE',
+			'BREAKTHROUGH (Korean Ver.)'
+		]
+	},
+	{
+		title: 'FANCY YOU',
+		release: 'April 22, 2019',
+		art: 'fancyyou',
+		tracklist: [
+			'FANCY',
+			'STUCK IN MY HEAD',
+			'GIRLS LIKE US • Jihyo',
+			'HOT • Momo',
+			'TURN IT UP • Sana',
+			'STRAWBERRY • Chaeyoung'
+		]
+	},
+	{
+		title: '#TWICE2',
+		release: 'March 6, 2019',
+		art: 'twice2',
+		tracklist: [
+			'LIKEY (Japanese Ver.)',
+			'Heart Shaker (Japanese Ver.)',
+			'What is Love? (Japanese Ver.)',
+			'Dance The Night Away (Japanese Ver.)',
+			'YES or YES (Japanese Ver.)'
+		]
+	},
+	{
+		title: 'BDZ -Repackage-',
+		release: 'December 26, 2018',
+		art: 'bdzrepackage',
+		tracklist: [
+			'STAY BY MY SIDE',
+			'BDZ',
+			'One More Time',
+			'Candy Pop',
+			'L.O.V.E',
+			'Wishing',
+			'Say it again',
+			'Wake Me Up',
+			'BRAND NEW GIRL',
+			'Be As ONE',
+			'I WANT YOU BACK'
+		]
+	},
+	{
+		title: 'The year of "YES"',
+		release: 'December 12, 2018',
+		art: 'yearofyes',
+		tracklist: [
+			'The Best Thing I Ever Did',
+			'Be as ONE (Korean Ver.)',
+			'YES or YES',
+			'SAY YOU LOVE ME',
+			'LALALA • Jeongyeon',
+			'YOUNG & WILD • Chaeyoung',
+			'SUNSET • Jihyo',
+			'AFTER MOON',
+			'BDZ (Korean Ver.)'
+		]
+	},
+	{
+		title: 'YES or YES',
+		release: 'November 5, 2018',
+		art: 'yesoryes',
+		tracklist: [
+			'YES or YES',
+			'SAY YOU LOVE ME',
+			'LALALA • Jeongyeon',
+			'YOUNG & WILD • Chaeyoung',
+			'SUNSET • Jihyo',
+			'AFTER MOON',
+			'BDZ (Korean Ver.)'
+		]
+	},
+	{
+		title: 'BDZ',
+		release: 'September 12, 2018',
+		art: 'bdz',
+		tracklist: [
+			'BDZ',
+			'One More Time',
+			'Candy Pop',
+			'L.O.V.E',
+			'Wishing',
+			'Say it again',
+			'Wake Me Up',
+			'BRAND NEW GIRL',
+			'Be As ONE',
+			'I WANT YOU BACK'
+		]
+	},
+	{
+		title: 'Summer Nights',
+		release: 'July 9, 2018',
+		art: 'summernights',
+		tracklist: [
+			'Dance The Night Away',
+			'CHILLAX',
+			'Shot thru the heart • Momo, Sana, Mina',
+			'What is Love?',
+			'SWEET TALKER • Jeongyeon, Chaeyoung',
+			'HO! • Jihyo',
+			'DEJAVU',
+			'SAY YES',
+			'STUCK'
+		]
+	},
+	{
+		title: 'What is Love?',
+		release: 'April 10, 2018',
+		art: 'whatislove',
+		tracklist: [
+			'What is Love?',
+			'SWEET TALKER • Jeongyeon, Chaeyoung',
+			'HO! • Jihyo',
+			'DEJAVU',
+			'SAY YES',
+			'STUCK'
+		]
+	},
+	{
+		title: 'Merry & Happy',
+		release: 'December 11, 2017',
+		art: 'merryhappy',
+		tracklist: [
+			'Heart Shaker',
+			'Merry & Happy',
+			'LIKEY',
+			'TURTLE',
+			'MISSING U • Dahyun, Chaeyoung',
+			'WOW',
+			'FFW',
+			'DING DONG',
+			'24/7 • Nayeon, Jihyo',
+			'LOOK AT ME',
+			"ROLLIN'",
+			'LOVE LINE • Jeongyeon',
+			"DON'T GIVE UP • Chaeyoung",
+			'YOU IN MY HEART',
+			'JALJAYO GOOD NIGHT'
+		]
+	},
+	{
+		title: 'twicetagram',
+		release: 'October 30, 2017',
+		art: 'twicetagram',
+		tracklist: [
+			'LIKEY',
+			'TURTLE',
+			'MISSING U • Dahyun, Chaeyoung',
+			'WOW',
+			'FFW',
+			'DING DONG',
+			'24/7 • Nayeon, Jihyo',
+			'LOOK AT ME',
+			"ROLLIN'",
+			'LOVE LINE • Jeongyeon',
+			"DON'T GIVE UP • Chaeyoung",
+			'YOU IN MY HEART',
+			'JALJAYO GOOD NIGHT'
+		]
+	},
+	{
+		title: '#TWICE',
+		release: 'June 28, 2017',
+		art: 'twice',
+		tracklist: [
+			'Like OOH-AHH (Japanese Ver.)',
+			'CHEER UP (Japanese Ver.)',
+			'TT (Japanese Ver.)',
+			'KNOCK KNOCK (Japanese Ver.)',
+			'SIGNAL (Japanese Ver.)'
+		]
+	},
+	{
+		title: 'SIGNAL',
+		release: 'May 15, 2017',
+		art: 'signal',
+		tracklist: [
+			'SIGNAL',
+			'THREE TIMES A DAY',
+			'Only YOU',
+			'HOLD ME TIGHT',
+			'EYE EYE EYES • Jihyo, Chaeyoung',
+			'SOMEONE LIKE ME'
+		]
+	},
+	{
+		title: 'TWICEcoaster LANE 2',
+		release: 'Febuary 20, 2017',
+		art: 'twicecoaster2',
+		tracklist: [
+			'KNOCK KNOCK',
+			'Ice Cream',
+			'TT',
+			'1 TO 10',
+			'PONYTAIL',
+			'JELLY JELLY',
+			'PIT-A-PAT',
+			'NEXT PAGE',
+			'ONE IN A MILLION'
+		]
+	},
+	{
+		title: 'TWICEcoaster LANE 1',
+		release: 'October 24, 2016',
+		art: 'twicecoaster1',
+		tracklist: [
+			'TT',
+			'1 TO 10',
+			'PONYTAIL',
+			'JELLY JELLY',
+			'PIT-A-PAT',
+			'NEXT PAGE',
+			'ONE IN A MILLION'
+		]
+	},
+	{
+		title: 'PAGE TWO',
+		release: 'April 25, 2016',
+		art: 'pagetwo',
+		tracklist: [
+			'Cheer Up',
+			'Precious Love • Chaeyoung',
+			'Touchdown',
+			'Tuk Tok',
+			'Woohoo',
+			'My Headphones on'
+		]
+	},
+	{
+		title: 'THE STORY BEGINS',
+		release: 'October 20, 2015',
+		art: 'thestorybegins',
+		tracklist: [
+			'Like OOH-AHH',
+			'Do it again',
+			'Going Crazy',
+			'Truth',
+			'Candy Boy',
+			'Like a Fool'
+		]
+	}
+]
--- src/pages/Discography.jsx
+++ src/pages/Discography.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react'
+import { albums } from '../data/albums'
+// import ProfileModal from '../components/ProfileModal'
 import '../style/page/discography.css'
 
-export default function Discography() {
-	return (
-		<main>
-			Discography
-		</main>
-	)
+export default function Profile() {
+  // const [activeMember, setActiveMember] = useState(null)
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <div className='thumb_container'>
+        {albums.map((album, index) => {
+          return (
+            <img
+              key={album.title}
+              alt={album.title}
+              src={index <= count ? `/images/discography/${album.art}.jpeg` : undefined}
+              onLoad={e => {
+                e.currentTarget.classList.add('loaded')
+                setCount(c => c + 1)
+              }}
+              width={700}
+              height={700}
+              // onClick={() => setActiveMember(member)}
+            />
+          )
+        })}
+      </div>
+
+      {/* {activeMember && (<ProfileModal member={activeMember} onClose={() => setActiveMember(null)} />)} */}
+    </main>
+  )
 }
