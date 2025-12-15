322c30539384a1ae22880d34940e02cd47fabb3f
Douglas Kadlec <colorghost@outlook.com>
2025-12-14 17:10:13 -0800

    Create discography modal

---
+++ src/components/DiscographyModal.jsx
@@ -0,0 +1,27 @@
+export default function DiscographyModal({ album, onClose }) {
+	const renderTrack = track => {
+		const index = track.indexOf('•')
+		if (index === -1) {
+			return <div className='track' key={track}>{track}</div>
+		}
+		return (
+			<div key={track}>
+				<span className='track'>{track.slice(0, index)}</span>
+				<span className='author'>{track.slice(index)}</span>
+			</div>
+		)
+	}
+
+	return (
+		<div className='modal' onClick={onClose}>
+			<div className='grid_container'>
+				<img src={`images/discography/${album.art}.jpeg`} alt={album.title} />
+				<div className='text_detail frost'>
+					<div className='title'>{album.title}</div>
+					<div className='release'>{album.release}</div>
+					<div className='tracklist'>{album.tracklist.map(renderTrack)}</div>
+				</div>
+			</div>
+		</div>
+	)
+}
--- src/pages/Discography.jsx
+++ src/pages/Discography.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
 import { albums } from '../data/albums'
-// import ProfileModal from '../components/ProfileModal'
+import DiscographyModal from '../components/DiscographyModal'
 import '../style/page/discography.css'
 
-export default function Profile() {
-  // const [activeMember, setActiveMember] = useState(null)
-  const [count, setCount] = useState(0)
+export default function Discography() {
+	const [activeAlbum, setActiveAlbum] = useState(null)
+	const [count, setCount] = useState(0)
 
-  return (
-    <main>
-      <div className='thumb_container'>
-        {albums.map((album, index) => {
-          return (
-            <img
-              key={album.title}
-              alt={album.title}
-              src={index <= count ? `/images/discography/${album.art}.jpeg` : undefined}
-              onLoad={e => {
-                e.currentTarget.classList.add('loaded')
-                setCount(c => c + 1)
-              }}
-              width={700}
-              height={700}
-              // onClick={() => setActiveMember(member)}
-            />
-          )
-        })}
-      </div>
+	return (
+		<main>
+			<div className='thumb_container'>
+				{albums.map((album, index) => {
+					return (
+						<img
+							key={album.title}
+							alt={album.title}
+							src={index <= count ? `/images/discography/${album.art}.jpeg` : undefined}
+							onLoad={e => {
+								e.currentTarget.classList.add('loaded')
+								setCount(c => c + 1)
+							}}
+							width={700}
+							height={700}
+							onClick={() => setActiveAlbum(album)}
+						/>
+					)
+				})}
+			</div>
 
-      {/* {activeMember && (<ProfileModal member={activeMember} onClose={() => setActiveMember(null)} />)} */}
-    </main>
-  )
+			{activeAlbum && (<DiscographyModal album={activeAlbum} onClose={() => setActiveAlbum(null)} />)}
+		</main>
+	)
 }
