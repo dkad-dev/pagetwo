3285355247be344147c3a6475d2d0399c9969cd8
Douglas Kadlec <colorghost@outlook.com>
2025-12-15 14:38:17 -0800

    Fix syntax error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename)
 const app = express()
 const distPath = path.join(__dirname, 'dist')
 
-app.use(express.static(distPath), { maxAge: '1y', immutable: true })
+app.use(express.static(distPath, { maxAge: '1y', immutable: true }))
 
 app.use((req, res) => {
 	res.set('Cache-Control', 'no-cache').sendFile(path.join(distPath, 'index.html'))
