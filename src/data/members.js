cf450aa2994c3775231732cd4735c6c2c4cf7c24
Douglas Kadlec <colorghost@outlook.com>
2025-12-15 09:39:29 -0800

    Create photo data and reformat albums and members data

--- src/data/members.js
+++ src/data/members.js
@@ -1,65 +1,65 @@
 export const members = [
 	{
-		name: 'nayeon',
-		birth: 'September 22, 1995',
-		nation: 'South Korea',
+		name: "nayeon",
+		birth: "September 22, 1995",
+		nation: "South Korea",
 		intro: "As the unofficial co-leader and face of TWICE, Nayeon plays a pivotal role both in the studio and on stage. She's known for her bright, energetic presence, effortless charisma, and gift of gab. ",
-		id: '28Avip-paeA'
+		id: "28Avip-paeA"
 	},
 	{
-		name: 'jeongyeon',
-		birth: 'November 1, 1996',
-		nation: 'South Korea',
+		name: "jeongyeon",
+		birth: "November 1, 1996",
+		nation: "South Korea",
 		intro: "Jeongyeon stands out with her powerful and distinctive vocals, often taking on key vocal roles in TWICE's music. Off stage, she is endeared for her cool, caring, and down-to-earth personality.",
-		id: 't4eIRbA4zw8'
+		id: "t4eIRbA4zw8"
 	},
 	{
-		name: 'momo',
-		birth: 'November 9, 1996',
-		nation: 'Japan',
+		name: "momo",
+		birth: "November 9, 1996",
+		nation: "Japan",
 		intro: "Momo is arguably the group's most talented dancer, celebrated for her precise and powerful moves. Her deliberate flow and flawless choreography solidify her as a commanding leader on stage.",
-		id: 'fDwoc1darYk'
+		id: "fDwoc1darYk"
 	},
 	{
-		name: 'sana',
-		birth: 'December 29, 1996',
-		nation: 'Japan',
+		name: "sana",
+		birth: "December 29, 1996",
+		nation: "Japan",
 		intro: "Sana is renowned for her infectious charm and lively personality, often drawing attention for her playful interactions. Despite her popularity, Sana has been regarded as TWICE's most humble member.",
-		id: 'CPcvaBmrD60'
+		id: "CPcvaBmrD60"
 	},
 	{
-		name: 'jihyo',
-		birth: 'February 1, 1997',
-		nation: 'South Korea',
-		intro: "Jihyo, the official leader of TWICE, is known for her strong leadership skills and dedication to the group's well-being. Jihyo is praised for her maturity and calm demeanor, both on and off the stage.",
-		id: 'CYicpmxKyME'
+		name: "jihyo",
+		birth: "February 1, 1997",
+		nation: "South Korea",
+		intro: "Jihyo, the official leader of TWICE, is known for her strong leadership skills and dedication to the group's success. Jihyo is praised for her maturity and calm demeanor, both on and off the stage.",
+		id: "CYicpmxKyME"
 	},
 	{
-		name: 'mina',
-		birth: 'March 24, 1997',
-		nation: 'Japan',
+		name: "mina",
+		birth: "March 24, 1997",
+		nation: "Japan",
 		intro: "Mina is admired for her graceful and elegant presence, often standing out with her poised demeanor. The fluidity and precision in her movements can be traced back to a decade of ballet training.",
-		id: '7kV7urmtsWE'
+		id: "7kV7urmtsWE"
 	},
 	{
-		name: 'dahyun',
-		birth: 'May 28, 1998',
-		nation: 'South Korea',
+		name: "dahyun",
+		birth: "May 28, 1998",
+		nation: "South Korea",
 		intro: "Dahyun is known for her bright and lively personality, often bringing a playful energy to TWICE's performances. Beyond a wide range of musical talents, Dahyun is beloved for her sense of humor and humble nature.",
-		id: 'xBDECHE0Qig'
+		id: "xBDECHE0Qig"
 	},
 	{
-		name: 'chaeyoung',
-		birth: 'April 23, 1999',
-		nation: 'South Korea',
+		name: "chaeyoung",
+		birth: "April 23, 1999",
+		nation: "South Korea",
 		intro: "Chaeyoung is the group's main creative, known for her versatility and confident delivery. Chaeyoung's quiet and thoughtful nature contrasts with her bold performances and daring artwork.",
-		id: 'eMpMgEyp7yg'
+		id: "eMpMgEyp7yg"
 	},
 	{
-		name: 'tzuyu',
-		birth: 'June 14, 1999',
-		nation: 'Taiwan',
+		name: "tzuyu",
+		birth: "June 14, 1999",
+		nation: "Taiwan",
 		intro: "Tzuyu, the youngest member, captivates audiences with her visuals and ethereal presence. Despite her reserved nature, her studio recordings reveal a confident, powerful, and authentic individual.",
-		id: 'sgey4Z91Xd8'
+		id: "sgey4Z91Xd8"
 	}
 ]
---
+++ src/data/photos.js
@@ -0,0 +1,332 @@
+export const photos = [
+	{
+		title: "nayeon7",
+		width: "1200",
+		height: "858"
+	},
+	{
+		title: "jeongyeon7",
+		width: "1200",
+		height: "858"
+	},
+	{
+		title: "momo7",
+		width: "1200",
+		height: "858"
+	},
+	{
+		title: "sana7",
+		width: "1200",
+		height: "858"
+	},
+	{
+		title: "jihyo7",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "mina7",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "dahyun7",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "chaeyoung7",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "tzuyu7",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "nayeon6",
+		width: "1000",
+		height: "714"
+	},
+	{
+		title: "jeongyeon6",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "momo6",
+		width: "1200",
+		height: "857"
+	},
+	{
+		title: "sana6",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "jihyo6",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "mina6",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "dahyun6",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "chaeyoung6",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "tzuyu6",
+		width: "1000",
+		height: "1400"
+	},
+	{
+		title: "twice6",
+		width: "1200",
+		height: "857"
+	},
+	{
+		title: "nayeon4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "jeongyeon4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "momo4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "sana4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "jihyo4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "mina4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "dahyun4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "chaeyoung4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "tzuyu4",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "twice4",
+		width: "1200",
+		height: "800"
+	},
+	{
+		title: "nayeon3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "jeongyeon3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "momo3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "sana3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "jihyo3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "mina3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "dahyun3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "chaeyoung3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "tzuyu3",
+		width: "800",
+		height: "1206"
+	},
+	{
+		title: "twice3",
+		width: "1200",
+		height: "800"
+	},
+	{
+		title: "nayeon2",
+		width: "1000",
+		height: "1500"
+	},
+	{
+		title: "jeongyeon2",
+		width: "1000",
+		height: "1500"
+	},
+	{
+		title: "momo2",
+		width: "1000",
+		height: "1500"
+	},
+	{
+		title: "sana2",
+		width: "1200",
+		height: "800"
+	},
+	{
+		title: "jihyo2",
+		width: "1000",
+		height: "1500"
+	},
+	{
+		title: "mina2",
+		width: "1000",
+		height: "1500"
+	},
+	{
+		title: "dahyun2",
+		width: "1000",
+		height: "1500"
+	},
+	{
+		title: "chaeyoung2",
+		width: "1000",
+		height: "1500"
+	},
+	{
+		title: "tzuyu2",
+		width: "1000",
+		height: "1500"
+	},
+	{
+		title: "nayeon1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "jeongyeon1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "momo1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "sana1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "jihyo1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "mina1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "dahyun1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "chaeyoung1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "tzuyu1",
+		width: "1207",
+		height: "1508"
+	},
+	{
+		title: "nayeon0",
+		width: "814",
+		height: "1200"
+	},
+	{
+		title: "jeongyeon0",
+		width: "841",
+		height: "1200"
+	},
+	{
+		title: "momo0",
+		width: "814",
+		height: "1200"
+	},
+	{
+		title: "sana0",
+		width: "800",
+		height: "1200"
+	},
+	{
+		title: "jihyo0",
+		width: "817",
+		height: "1200"
+	},
+	{
+		title: "mina0",
+		width: "800",
+		height: "1200"
+	},
+	{
+		title: "dahyun0",
+		width: "815",
+		height: "1200"
+	},
+	{
+		title: "chaeyoung0",
+		width: "800",
+		height: "1200"
+	},
+	{
+		title: "tzuyu0",
+		width: "809",
+		height: "1200"
+	}
+]
