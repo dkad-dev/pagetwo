fa735baff4bc8a735a531cbf3b25ba3289be84cc
Douglas Kadlec <colorghost@outlook.com>
2025-12-16 15:36:13 -0800

    Create transparent logo and update header/footer transparency settings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
 	return (
 		<footer>
-			<Link to='/'><img className='logo' src='/icons/logo.png' alt='logo' /></Link>
+			<Link to='/'><img className='logo' src='/icons/logo_transparent.png' alt='logo' /></Link>
 		</footer>
 	)
 }
